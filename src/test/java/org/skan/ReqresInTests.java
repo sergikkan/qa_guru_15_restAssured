fa7c7e90028634697dfaa9b40a93c82bf9b1ea9f
user <sergik-199022@mail.ru>
2022-12-11 01:25:10 +0600

    first commit

---
+++ src/test/java/org/skan/ReqresInTests.java
@@ -0,0 +1,112 @@
+package org.skan;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTests {
+
+    //{ "id": 4, "token": "QpwL5tke4Pnpja7X4" }
+    @Test
+    void registerTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"))
+                .body("id", is(4));
+    }
+
+    @Test
+    void registerWithoutPassTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void registerWithoutEmailTest() {
+        String data = "{ \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void CheckUsersPerPage() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=1")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("per_page", is(6));
+    }
+
+    //"id": 2,
+    //        "email": "janet.weaver@reqres.in",
+    //        "first_name": "Janet",
+    //        "last_name": "Weaver",
+    @Test
+    void findExistUser() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"));
+    }
+
+    @Test
+    void userNotFound() {
+
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+}
